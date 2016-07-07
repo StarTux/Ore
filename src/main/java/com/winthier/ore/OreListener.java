e35170d1064ccbfdb41585adc0fb6d8b3d0c92ca
Thomas Loy <ths.loy@googlemail.com>
2016-07-07 16:03:59 +0200

    Quick fix for a timing error where placed stone could bring back harvested ores

--- src/main/java/com/winthier/ore/OreListener.java
+++ src/main/java/com/winthier/ore/OreListener.java
@@ -1,5 +1,7 @@
 package com.winthier.ore;
 
+import java.util.HashSet;
+import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
@@ -7,16 +9,19 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockDamageEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 @RequiredArgsConstructor
 public class OreListener implements Listener {
     final OrePlugin plugin;
+    final Set<Block> placedBlocks = new HashSet<>();
     
     @EventHandler(ignoreCancelled = false, priority = EventPriority.LOWEST)
     public void onPlayerInteract(PlayerInteractEvent event) {
         Block block = event.getClickedBlock();
         if (block == null) return;
+        if (placedBlocks.contains(block)) return;
         WorldGenerator worldGen = plugin.generators.get(block.getWorld().getName());
         if (worldGen == null) return;
         worldGen.realize(block);
@@ -26,6 +31,7 @@ public class OreListener implements Listener {
     public void onBlockDamage(BlockDamageEvent event) {
         Block block = event.getBlock();
         if (block == null) return;
+        if (placedBlocks.contains(block)) return;
         WorldGenerator worldGen = plugin.generators.get(block.getWorld().getName());
         if (worldGen == null) return;
         worldGen.realize(block);
@@ -34,6 +40,7 @@ public class OreListener implements Listener {
     @EventHandler(ignoreCancelled = false, priority = EventPriority.LOWEST)
     public void onBlockBreak(BlockBreakEvent event) {
         final Block block = event.getBlock();
+        if (placedBlocks.contains(block)) return;
         WorldGenerator worldGen = plugin.generators.get(block.getWorld().getName());
         if (worldGen == null) return;
         final int R = 2;
@@ -46,5 +53,10 @@ public class OreListener implements Listener {
                 }
             }
         }
+    }
+
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.LOWEST)
+    public void onBlockPlace(BlockPlaceEvent event) {
+        placedBlocks.add(event.getBlock());
     }
 }
