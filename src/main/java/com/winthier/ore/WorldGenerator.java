063837333f6807aa02768ee68dacb20acd3d2eef
Thomas Loy <ths.loy@googlemail.com>
2016-10-10 22:10:44 +0200

    Bring back creepers in mini caves

--- src/main/java/com/winthier/ore/WorldGenerator.java
+++ src/main/java/com/winthier/ore/WorldGenerator.java
@@ -698,11 +698,12 @@ class WorldGenerator {
                     if (special == Special.DESERT || special == Special.MESA || special == Special.SAVANNA) {
                         state.setSpawnedType(EntityType.BLAZE);
                     } else {
-                        switch (random.nextInt(4)) {
+                        switch (random.nextInt(5)) {
                         case 0: state.setSpawnedType(EntityType.ZOMBIE); break;
                         case 1: state.setSpawnedType(EntityType.SKELETON); break;
                         case 2: state.setSpawnedType(EntityType.SPIDER); break;
                         case 3: state.setSpawnedType(EntityType.CAVE_SPIDER); break;
+                        case 4: state.setSpawnedType(EntityType.CREEPER); break;
                         }
                     }
                 } else if (mobCount > 0) {
